0d8648f0305a19394b1eb24821c707ad48e229ef
AKarp123 <ash.daboss111@gmail.com>
2024-09-17 01:42:32 -0400

    ok file upload actually works fr fr

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FilePond, FileStatus, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
-import { Button } from "@mui/material";
+import { Button, Paper } from "@mui/material";
 import axios from "axios";
 
 // Register the plugins
@@ -28,13 +28,19 @@ const Upload = () => {
     const handleUpload = async () => {
         const formData = new FormData();
 
-        let relativePath = files[0].relativePath.split("/")[0];
+        if(files.length === 0) {    
+            return;
+        }
 
 
         // Append each file along with its relative path to the FormData
         files.forEach(({ file, relativePath }, index) => {
             
             formData.set("relativePaths", relativePath);
+            if(relativePath !== formData.get("relativePaths")) {
+                
+                formData.append("relativePaths", relativePath);
+            }
             formData.append("files", file); // Append the file itself
             
           // Append the relative path
@@ -55,29 +61,66 @@ const Upload = () => {
         
     };
 
+    const handleFileUpload = (fieldName, file, metadata, load, error, progress, abort) => {
+
+        const formData = new FormData();
+        const relativePath = file.webkitRelativePath.split("/")[0];
+        formData.set("relativePaths", relativePath);
+        formData.append(fieldName, file, file.name);
+        console.log(metadata)
+        
+        /**
+         * 
+         * Note for future self, spent like 4 hours trying to bulk upload everything at once when i could've 
+         * just uploaded one at a time.
+         * and the code is 10x simpler and cleaner
+         * 
+         */
+
+        axios.post("/api/upload", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "Accept": "utf-8",
+            },
+            onUploadProgress: (e) => {
+                progress(e.lengthComputable, e.loaded, e.total);
+            },
+            cancelToken: new axios.CancelToken((c) => {
+                abort = c;
+            })
+
+        }).then((res) => {
+            load(res.data.file);
+        }).catch((err) => {
+            error("Upload failed");
+        }
+        
+        )
+        
+    }
+
     return (
-        <div>
+        
             <FilePond
                 files={files.map((file) => file.file)}
                 onupdatefiles={handleUpdateFiles}
                 allowMultiple={true}
                 chunkUploads={false}
-                instantUpload={false}
+                instantUpload={true}
                 server={{
-                    process: {
-                        url: "/api/upload",
-                        method: "POST",
-                    },
+                    process: handleFileUpload,
+                    load: null,
                     fetch: null,
                     revert: null,
                 }}
-                name="file"
+                name="files"
                 lableIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
                 allowDirectoriesOnly={true}
+                maxParallelUploads={10}
+                credits={false}
                 
             />
-            <Button onClick={handleUpload}>Upload</Button>
-        </div>
+          
     );
 };
 
