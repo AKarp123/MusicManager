6b35f0730527c5c92be011e7ae9bb364b18e890a
AKarp123 <ashtonkarp63@gmail.com>
2026-07-17 01:22:43 -0700

    feat: add readDirectory function to retrieve directory entries with type definitions

--- backend/src/utils/io.ts
+++ backend/src/utils/io.ts
@@ -5,6 +5,11 @@ import { resolve } from 'path'
  * Directory & File Utilities
  */
 
+export type DirectoryEntry = {
+	name: string
+	isDirectory: boolean
+}
+
 const baseDirectory = '/library'
 
 const normalizePath = (path: string): string => {
@@ -23,6 +28,27 @@ export const listDirectories = async (path: string): Promise<string[]> => {
 			.filter((entry) => entry.isDirectory())
 			.map((entry) => entry.name)
 		return directories
+	} catch (error) {
+		console.error(`Error reading directory ${path}:`, error)
+		throw error
+	}
+}
+/**
+ *
+ * @param path - Directory Path
+ * @returns List of file and directory names
+ */
+export const readDirectory = async (
+	path: string
+): Promise<DirectoryEntry[]> => {
+	try {
+		path = normalizePath(path)
+		const entries = await readdir(path, { withFileTypes: true })
+		const directoryEntries = entries.map((entry) => ({
+			name: entry.name,
+			isDirectory: entry.isDirectory()
+		}))
+		return directoryEntries
 	} catch (error) {
 		console.error(`Error reading directory ${path}:`, error)
 		throw error
